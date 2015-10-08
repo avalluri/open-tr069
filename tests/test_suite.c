9ccbce0e8ff3c9b13668ae2ac09ea4b58a6c1752
Amarnath Valluri <amarnath.valluri@intel.com>
2015-10-08 14:34:48 +0300

    tests: writing evcpe logs to file while running tests
    This makes tests ouput clean.
    
    Signed-off-by: Amarnath Valluri <amarnath.valluri@intel.com>

--- tests/test_suite.c
+++ tests/test_suite.c
@@ -20,20 +20,75 @@
 
 #include <mcheck.h>
 #include <getopt.h>
+#include <stdio.h>
 
 #include <textui/TextOutputter.h>
 #include <textui/TextUIRunner.h>
+#include <embUnit/TestResult.h>
+#include <embUnit/TestListener.h>
 
 #include "log.h"
 #include "test_suite.h"
 
+#define TESTS_LOG_FILE "evcpe-tests.log"
+static FILE* _fp = NULL;
+static OutputterRef _txtOutputter = NULL;
+
+static void _printHeader(OutputterRef self)
+{
+	Outputter_printHeader(_txtOutputter);
+}
+
+static void _printStartTest(OutputterRef self,TestRef test)
+{
+	fprintf(_fp, "+ Start of %s\n", Test_name(test));
+
+	Outputter_printStartTest(_txtOutputter, test);
+}
+
+static void _printEndTest(OutputterRef self,TestRef test)
+{
+	fprintf(_fp, "+ End of %s\n", Test_name(test));
+
+	Outputter_printEndTest(_txtOutputter, test);
+}
+
+static void _printSuccessful(OutputterRef self, TestRef test, int runCount)
+{
+	Outputter_printSuccessful(_txtOutputter, test, runCount);
+}
+
+static void _printFailure(OutputterRef self, TestRef test, char *msg, int line,
+		char *file,int runCount)
+{
+	Outputter_printFailure(_txtOutputter, test, msg, line, file, runCount);
+}
+
+static void _printStatistics(OutputterRef self,TestResultRef result)
+{
+	Outputter_printStatistics(_txtOutputter, result);
+}
+
+static const OutputterImplement _outputterImpl = {
+	(OutputterPrintHeaderFunction)		_printHeader,
+	(OutputterPrintStartTestFunction)	_printStartTest,
+	(OutputterPrintEndTestFunction)		_printEndTest,
+	(OutputterPrintSuccessfulFunction)	_printSuccessful,
+	(OutputterPrintFailureFunction)		_printFailure,
+	(OutputterPrintStatisticsFunction)	_printStatistics,
+};
+
+Outputter _outputter = {
+	(OutputterImplementRef)&_outputterImpl,
+};
+
 int main(int argc, char **argv)
 {
 	int c;
-	enum evcpe_log_level level;
+	enum evcpe_log_level level = EVCPE_LOG_INFO;
+	OutputterRef out = NULL;
 
-	level = EVCPE_LOG_INFO;
-	while ((c = getopt(argc, argv, "vs")) != -1)
+	while ((c = getopt(argc, argv, "vs")) != -1) {
 		switch (c) {
 		case 's':
 			level ++;
@@ -42,28 +97,42 @@ int main(int argc, char **argv)
 			if (level > EVCPE_LOG_TRACE)
 				level --;
 		}
+	}
 
 	mtrace();
 
-	if (level <= EVCPE_LOG_FATAL)
-		evcpe_add_logger("stderr", level, EVCPE_LOG_FATAL,
-				NULL, evcpe_file_logger, stdout);
+	if (!(_fp = fopen(TESTS_LOG_FILE, "w+"))) {
+		fprintf(stderr, "Failed to open log file: %s", TESTS_LOG_FILE);
+		_fp = stdout;
+	}
+
+	if (level <= EVCPE_LOG_FATAL) {
+		evcpe_add_logger("stderr", level, EVCPE_LOG_FATAL,NULL,
+				evcpe_file_logger, _fp);
+	}
 
 	INFO("starting test");
 
-	TextUIRunner_setOutputter(TextOutputter_outputter());
-	TextUIRunner_start();
+	_txtOutputter = TextOutputter_outputter();
+
+	if (_fp == stdout)
+		out = _txtOutputter;
+	else out = &_outputter;
+
+	TextUIRunner_startWithOutputter(out);
 	TextUIRunner_runTest(evcpe_msg_xml_test_case());
 	TextUIRunner_runTest(evcpe_obj_test_case());
 	TextUIRunner_runTest(evcpe_test_case());
 	TextUIRunner_runTest(evcpe_model_xml_test_case());
 	TextUIRunner_runTest(evcpe_url_test_case());
 	TextUIRunner_runTest(evcpe_cookie_test_case());
-	//TextUIRunner_runTest(evcpe_repo_test_case());
+	TextUIRunner_runTest(evcpe_repo_test_case());
 	TextUIRunner_end();
 
 	evcpe_remove_logger("stdout");
 	evcpe_remove_logger("stderr");
+
+	if (_fp != stdout) fclose(_fp);
 
 	muntrace();
 
